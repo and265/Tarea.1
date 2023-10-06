607610ae639f8a08c281be7bd4fb88c4e296fecf
and265 <131011527+and265@users.noreply.github.com>
2023-10-06 18:24:18 -0300

    Desarrollo clase Articulo

--- Tarea 1/src/Articulo.java
+++ Tarea 1/src/Articulo.java
@@ -1,2 +1,59 @@
 public class Articulo {
+
+    private float peso;
+
+    private String nombre;
+
+    private String descripcion;
+
+    private float precio;
+
+    public Articulo(float peso, String nombre, String descripcion, float precio) {
+
+        this.peso = peso;
+        this.nombre  = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+    @Override
+    public String toString() {
+        return "Articulo {" +
+                "Peso: " + peso +
+                " gramos, Nombre: " + nombre +
+                ", Descripcion: " + descripcion +
+                ", Precio: " + precio +
+                '}';
+    }
 }
