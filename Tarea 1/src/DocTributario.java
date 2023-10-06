9dbe947e4d220fcd6cca5bcba746c07eab783429
and265 <131011527+and265@users.noreply.github.com>
2023-10-06 20:06:27 -0300

    Desarrollo Clase DocTributario

--- Tarea 1/src/DocTributario.java
+++ Tarea 1/src/DocTributario.java
@@ -1,2 +1,48 @@
+import java.util.Date;
+
 public class DocTributario {
+
+    private String numero;
+    private String rut;
+    private Date fecha;
+
+    public DocTributario(String numero, String rut, Date fecha) {
+
+        this.numero = numero;
+        this.rut = rut;
+        this.fecha = fecha;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Documento Tributario {" +
+                "Numero: " + numero +
+                ", RUT: " + rut +
+                ", Fecha: " + fecha +
+                '}';
+    }
 }
