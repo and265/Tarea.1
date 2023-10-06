d123b61a3f4d5b2567ea74157dedf8354bb203bf
and265 <131011527+and265@users.noreply.github.com>
2023-10-06 20:22:44 -0300

    Desarrollo subclase Factura

--- Tarea 1/src/Factura.java
+++ Tarea 1/src/Factura.java
@@ -1,2 +1,18 @@
-public class Factura {
+import java.util.Date;
+
+public class Factura extends DocTributario{
+
+    public Factura(String n, String r, Date f) {
+        super(n, r, f);
+    }
+
+    @Override
+    public String toString() {
+        return "Factura {" +
+                "Numero: " + getNumero() +
+                ", RUT: " + getRut() +
+                ", Fecha: " + getFecha() +
+                '}';
+    }
+
 }
