5e62b779b8b9a8e6ee2d3677272b02bedb1cdcd7
and265 <131011527+and265@users.noreply.github.com>
2023-10-06 20:05:10 -0300

    Desarrollo Clase Dirección

--- Tarea 1/src/Direccion.java
+++ Tarea 1/src/Direccion.java
@@ -1,2 +1,26 @@
 public class Direccion {
+
+    private String direccion;
+
+    public Direccion(String direc) {
+
+        direccion = direc;
+    }
+
+    public String getDireccion(){
+
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Direccion {" +
+                "Direccion: " + direccion +
+                '}';
+    }
 }
+
