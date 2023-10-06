248ad5bafab0841c44f8b4cbf616afb98ad9c76b
and265 <131011527+and265@users.noreply.github.com>
2023-10-06 20:07:58 -0300

    Desarrollo Clase DetalleOrden

--- Tarea 1/src/DetalleOrden.java
+++ Tarea 1/src/DetalleOrden.java
@@ -1,2 +1,56 @@
 public class DetalleOrden {
+    private int cantidad;
+    private Articulo articulo;
+
+    public DetalleOrden(Articulo articulo, int cantidad) {
+
+        this.cantidad = cantidad;
+        this.articulo = articulo;
+    }
+
+    public float calcPrecio(){
+
+        return cantidad * articulo.getPrecio();
+    }
+
+    public float calcPrecioSinIVA(){
+
+        return cantidad * articulo.getPrecio() * 100/119;
+    }
+
+    public float calcIVA(){
+
+        return cantidad * articulo.getPrecio() * 19/119;
+    }
+
+    public float calcPeso(){
+
+        return cantidad * articulo.getPeso();
+    }
+
+
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public Articulo getArticulo() {
+        return articulo;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setArticulo(Articulo articulo) {
+        this.articulo = articulo;
+    }
+
+    @Override
+    public String toString() {
+        return "DetalleOrden {" +
+                "Articulo: " + articulo.getNombre() +
+                ", Cantidad: " + cantidad +
+                " unidades}";
+    }
 }
