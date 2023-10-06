72f4f212563fd40e41d35326ae32d04fc006b837
and265 <131011527+and265@users.noreply.github.com>
2023-10-06 20:18:18 -0300

    Desarrollo subclase Efectivo

--- Tarea 1/src/Efectivo.java
+++ Tarea 1/src/Efectivo.java
@@ -1,2 +1,28 @@
-public class Efectivo {
+import java.util.Date;
+
+public class Efectivo extends Pago{
+    public Efectivo(float m, Date f) {
+        super(m, f);
+    }
+
+    public float calcDevolucion(OrdenCompra oc){
+
+        if ( getMonto() > oc.getDeuda()){
+
+            return getMonto() - oc.calcPrecio();
+        }
+
+        else
+            return 0;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Efectivo {" +
+                "Monto: " + getMonto() +
+                ", Fecha: " + getFecha() +
+                '}';
+    }
+
 }
